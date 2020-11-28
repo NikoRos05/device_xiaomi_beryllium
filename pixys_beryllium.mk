6fde7a95f11156045f10a419ff38db3e6d1b985a
NikoRos05 <sorok310@gmail.com>
2020-11-28 21:27:10 +0300

    poco: pixys

--- pixys_beryllium.mk
+++ pixys_beryllium.mk
@@ -12,13 +12,13 @@ $(call inherit-product, device/xiaomi/beryllium/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
-# Inherit some common Havoc stuff.
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
-HAVOC_MAINTAINER := Ninad Patil (REIGNZ3)
-HAVOC_GROUP_URL := https://t.me/havoc_beryllium
+# Inherit some common Pixys stuff.
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
+PIXYS_MAINTAINER := NikoRos05
+PIXYS_GROUP_URL := https://t.me/roms_by_nikoros05
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := havoc_beryllium
+PRODUCT_NAME := pixys_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
